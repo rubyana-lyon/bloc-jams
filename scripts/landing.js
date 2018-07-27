1bea52b1bfc9cb684d0c672a4a88df45402cd838
rubyana88 <rubyanadesign@gmail.com>
2018-07-27 16:44:03 -0400

    checkpoint 23 events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,7 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+ var pointsArray = document.getElementsByClassName('point');
+
+ var animatePoints = function() {
+     var points = document.getElementsByClassName('point');
 
      var revealPoint = function(index) {
          points[index].style.opacity = 1;
@@ -7,7 +9,24 @@ var animatePoints = function() {
          points[index].style.msTransform = "scaleX(1) translateY(0)";
          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
      }
+
      for (var i = 0; i < points.length; i++) {
          revealPoint(i);
      }
- };
+};
+
+window.onload = function() {
+        // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+        if (window.innerHeight > 950) {
+            animatePoints(pointsArray);
+        }
+
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+     });
+}
