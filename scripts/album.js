a5365f35173db7c8cf724b5e2ee313c228d13bd5
rubyana88 <rubyanadesign@gmail.com>
2018-07-28 11:22:33 -0400

    checkpoint 26 play button completed

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@ var albumCharlie = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -81,8 +81,21 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumCharlie);
+    songListContainer.addEventListener('mouseover', function(event) {
+        // Only target individual song rows during event delegation
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            }
+        });
     var albums = [albumPicasso, albumMarconi, albumCharlie];
     var index = 1;
     albumImage.addEventListener("click", function(event){
@@ -90,6 +103,11 @@ window.onload = function() {
         index++;
         if (index == albums.length) {
             index = 0;
-        }
-    });
-};
+            }
+        });
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+}
